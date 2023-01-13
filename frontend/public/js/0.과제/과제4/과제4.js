00c67f2da247d7fcbef73b8749de510f7ce9c7bb
Sengah Choi <cntjr3@naver.com>
2023-01-13 18:46:39 +0900

    구현해야할 기능 2개 남음

--- frontend/public/js/0.과제/과제4/과제4.js
+++ frontend/public/js/0.과제/과제4/과제4.js
@@ -24,14 +24,13 @@ function printContent( ){
 					<th>비고</th>
 				</tr>`
 	for ( let i = 0 ; i < 도서목록.length; i++ ){ // for s
-		
-		html += `<tr>
+			html += `<tr>
 					<td>${ i+1 }</td>
 					<td>${ 도서목록[i] }</td>
 					<td>대여가능여부</td>
 					<td><button onclick="Delete( ${i} )">삭제</button></td>
 				</tr>`	
-		
+
 	} // for e
 	document.querySelector('.result_box1').innerHTML = html
 } // f e
@@ -41,6 +40,12 @@ function 등록하기 ( 등록 ){
 	// 1. 중복검사/취소
 	if (도서목록.indexOf( 등록 ) >= 0 ) {
 		alert('도서명이 이미 등록되어있습니다. [등록되지 않습니다.]')
+		도서목록.splice( 도서목록.indexOf(등록), 1 )
+		printContent()
+		return;
+	}
+	if ( 등록.length < 5 || 등록.length > 10){
+		alert('글자 길이[5-10]를 맞춰주세요.')
 		도서목록.splice( 도서목록.indexOf(등록), 1 )
 		printContent()
 		return;
@@ -55,26 +60,3 @@ function 등록하기 ( 등록 ){
 구현해야할 기능
 1. 도서대여여부 -> 배열과 비교해서 있으면 대여중, 없으면 대여가능으로 출력
 2. 도서대여여부가 대여중일 경우 삭제 버튼 비활성화
-3. 글자수 5-10 제한
-	if ( 등록.length < 5 && 등록.length > 10){
-		alert('글자 길이[5-10]를 맞춰주세요.')
-		도서목록.splice( 도서목록.indexOf(등록), 1 )
-		printContent()
-		return;
-	}*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
