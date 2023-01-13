aa53681ae7ed66936fac5f066921281ca85db001
Sengah Choi <cntjr3@naver.com>
2023-01-13 12:20:34 +0900

    Day11 로또구매

--- frontend/public/js/2.함수/4.로또구매.js
+++ frontend/public/js/2.함수/4.로또구매.js
@@ -1,44 +1,95 @@
-console.log('js 작동'); // 스크립트 확인
-
-// 1. 숫자 버튼 출력하는 함수
-function 버튼출력(){ // f s
+// 1. 숫자 버튼 출력하는 함수 
+function 버튼출력(){ // f s 
+	let 버튼목록 = '';		// for 밖에 만든 이유 : 45개의 버튼을 저장할려고 
+	for( let i = 1 ; i<=45 ; i++ ){ // for s  // i는 1부터 45까지 1씩증가 반복처리 
+		
+		버튼목록 += `<button onclick="버튼클릭(${i})">${i}</button>`
+		
+		if( i % 5 == 0 ){ 버튼목록 += '<br/>'}  // 만약에 i가 5배수이면 줄바꿈
 	
-	console.log('2. 함수작동') // 함수확인
-	
-	let 버튼목록 = ''; // for 밖에 만든 이유 : 45개의 버튼을 누적 저장하려고
-	
-	// for
-	for (let i = 1 ; i <= 45 ; i ++ ) { // for s
-		console.log ( '3. : ' + i) // for문 확인
-				
-			버튼목록 += `<button onclick="버튼클릭(${i})">${i}</button>`
-			// 만약에 i가 5 배수일때마다 줄바꿈
-			if( i % 5 == 0 ){ 버튼목록 += '<br>'}
 	} // for e
-	console.log('4. : ' + 버튼목록) // 버튼목록 확인
 	
-	// 해당 <div>에 버튼목록 넣어주기
+	// 해당 <div> 에 버튼목록 넣어준다.
 	document.querySelector('.버튼목록상자').innerHTML = 버튼목록
 	
 	// 취소 버튼 보이게 활성화
 	document.querySelector('.취소버튼').style.display = 'inline'
 	
-} // f e
+} // f e 
 
-function 버튼출력취소( ){
-	// 해당 <div>에 ''공백으로 변경하기
+// 2. 숫자 버튼 없애는 함수
+function 버튼출력취소(){ // f s
+	// 해당 <div> 에 ''공백으로 변경하기 
 	document.querySelector('.버튼목록상자').innerHTML = ''
+	
 	// 취소 버튼 안보이게 비활성화
 	document.querySelector('.취소버튼').style.display = 'none'
-}
+	
+} // f e 
+
+// 1. 선택한 숫자를 6개 저장하는 배열
+let 선택번호목록 = [ ]
+
+// 3. 버튼 클릭 함수 
+function 버튼클릭( 번호 ){
+	console.log( 번호 +" 번호 를 선택했군요.")
+	// 1. 중복검사	/취소 [ 배열명.indexOf(찾을데이터) : 찾은데이터의 인덱스번호 반환 , 없으면 -1]
+	if( 선택번호목록.indexOf( 번호 ) >= 0 ){ // if s 
+		alert('이미 선택한 번호 입니다.[해당 버튼 취소 했습니다.]')
+		// [ 배열명.splice( 인덱스 , 개수 ) : 해당 인덱스부터 개수만큼 삭제 ]
+		선택번호목록.splice( 선택번호목록.indexOf(번호) , 1  )
+		추첨버튼출력() // 함수호출
+		return ;	// 반환값은 없지만 함수를 끝낼수 있다. [ 더이상 아래로 코드가 실행되지 않는다.]
+	} // if e
+	// 2. 6개까지만 저장 [ 배열명.length : 배열내 데이터 총 개수 ]
+	if( 선택번호목록.length == 6 ){	// 배열내 숫자가 6개 존재하면
+		alert('이미 모두[6개]를 선택했습니다.')
+		return ;	
+	} // if e 
+	// 위 2가지 유효성검사를 충족하지 않았을때 저장 
+	선택번호목록.push( 번호 )
+	추첨버튼출력(); // 함수호출
+} // f e 
+// 4. 선택번호목록 출력 및 추첨버튼 활성화
+function 추첨버튼출력( ){
+	// 추첨버튼 활성화/비활성화
+	if( 선택번호목록.length == 6  ){
+		document.querySelector('.추첨버튼').style.display = 'inline'
+	}else{
+		document.querySelector('.추첨버튼').style.display = 'none'
+	}
+	document.querySelector('.선택번호상자').innerHTML=선택번호목록
+} 
 
 
 
-	// 1. 문자열 처리
-			// '<button onclick="버튼클릭('+i+')">'+i+'</button>'
-			// 'html 문자열 ~~~~' + 변수 + 'html문자열' + 변수 + 
-		
-		// 2. 백틱 이용한 문자열처리
-			// `( 백틱 - esc 아래 )
-			// 1. 전체를 백틱으로 감싼다.
-			// 2. 변수/수식/함수호출은  ${변수/수식} 감싼다.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// 1. 문자열 처리 
+		// '<button onclick="버튼클릭('+i+')">'+i+'</button>
+		// 'html문자열~~~~~~~~~~~~'+변수+'html문자열'+변수+'html문자열
+	// 2. 백틱 이용한 문자열처리
+		//  `(백틱 - esc 아래 ) : 작은따옴표 아님
+		// 1. 전체를 백틱으로 감싼다.
+		// 2. 변수/수식 는 ${변수/수식} 감싼다.
+
