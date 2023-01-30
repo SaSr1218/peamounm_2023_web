a381277e7a4b29d723c41b9088d7ffac57f61f33
Sengah Choi <cntjr3@naver.com>
2023-01-30 15:11:03 +0900

    day 19 write 썸머노트 적용 완료

---
+++ jsp/src/main/webapp/board/write.js
@@ -0,0 +1,16 @@
+console.log('글쓰기js 실행')
+
+
+$(document).ready(function() {
+  $('#summernote').summernote( {
+	   height : 300, 
+	   lang: 'ko-KR' 
+  });
+});
+
+/*
+	썸머노트 기능 속성
+	  $('#summernote').summernote( {
+	   height : 300, 
+	   lang: 'ko-KR' 
+*/
