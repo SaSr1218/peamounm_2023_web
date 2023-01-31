22d637667fe29cc4bc45d68d0a2ca793e2eb99a5
Sengah Choi <cntjr3@naver.com>
2023-01-31 12:01:51 +0900

    Day 20 캘린더 일정 추가

--- jsp/src/main/webapp/js/member/calender.js
+++ jsp/src/main/webapp/js/member/calender.js
@@ -1,4 +1,13 @@
 
+let contents = [
+	{ date: '20230101' , content : '인문학 명저 독서'} ,
+	{ date: '20230104' , content : '집안 대청소'} ,
+	{ date: '20230107' , content : '코딩 공부하기, 정처기 공부하기, 백준 공부하기'}
+]
+
+
+
+/* --------------------전역변수 : 모든 함수{ } 공용으로 사용되는 메모리 [변수] --------- */
 // 1. js 열렸을때 현재 연도와 월을 구해서 변수에 저장
 let year = new Date().getFullYear(); // 현재 연도
 let month = new Date().getMonth()+1; // 현재 월
@@ -19,18 +28,44 @@ function cal_print(){
 	// * 현재 캘린더 설정된 날짜의 시작 요일 구하기
 	let weekday = new Date ( year , month-1 , 1 ).getDay(); console.log('현재 캘린더 시작일 : ' +  weekday)
 	
-	// * 2. 시작 요일 전에 공백 채우기
+	// * 4.2. 시작 요일 전에 공백 채우기
 	for (let b = 1 ; b<=weekday ; b++){
 		html += `<div class="day"></div>`
 	}
-	// * 1. 일 만들기 [ 1 ~ 마지막 일[월마다 다름] 까지 ]
+	// * 4.1. 일 만들기 [ 1 ~ 마지막 일[월마다 다름] 까지 ]
 	for (let day =1; day<=lastday; day++){
-		html += `<div class="day"> ${day} </div> `
+		
+		// 4. 날짜 확인
+		let date = new Date( year , month-1 , day );
+		let fdate = date_format( date );
+		
+		html += `<div class="day"> ${day} ${ contents_print( fdate ) } </div> `
 	}
-				
-				
-	// 4. 마크업에 출력
+	// 3. 마크업에 출력
 	document.querySelector('.cal_day').innerHTML = html
+}
+// 5. 일정 출력 함수
+function contents_print(fdate){
+	console.log(fdate)
+	// 1. 인수로 전달된 날짜와 동일한 일정 날짜 찾기
+		// 1. html
+		let html = ``
+	contents.forEach( (o) => { // 일정목록 반복문
+		if(fdate == o.date){ // 만약에 인수로 전달된 날짜와 일정목록에서 동일한 날짜가 존재하면
+			html += `<div class="content">${o.content}</div>`
+		}
+	}) // for end
+	return html;
+}
+
+// 4. 날짜 포맷 함수 [ 인수 : 날짜 ---- 로직[포맷] ----> 반환 : 변경된 날짜형식]
+function date_format( date ){
+	
+	let d_year = date.getFullYear();
+		// 만약에 월/요일이 한자리수(<=9)이면 앞에 0 붙이기
+	let d_month = (date.getMonth()+1) <= 9 ? '0' + (date.getMonth()+1) : (date.getMonth()+1) ;
+	let d_day = date.getDate() <= 9 ? '0' + date.getDate() : date.getDate();
+	return `${d_year}${d_month}${d_day}`
 }
 
 // 3. 이전달 버튼 클릭 이벤트에 따른 연도와 월이 변경
@@ -48,11 +83,14 @@ document.querySelector('.nextbtn').addEventListener( 'click', (e) => {
 })
 
 
+
+
 /*
 	new Date() 날짜/시간 관련 클래스
 		1. let date = new Date() 				: 현재 날짜 / 시간 객체
 		2. let date = new Date( 2023, 01, 31 )  : 사용자정의 날짜/시간 객체
-		3. let date = new Date( 연도, 월, 0 )	: 연도와 월 날짜에 따른 마지막 일 날짜
+		3. let date = new Date( 연도, 월, 0 )		: 연도와 월 날짜에 따른 마지막 일 날짜( 월 변수에 +1 했을 경우!)
+		3. let date = new Date( 연도, 월-1, 1 )	: 연도와 월 날짜에 따른 1일 날짜	 ( 월 변수에 +1 했을 경우!)
 			1. get 함수 제공
 				1. date.getFullYear()	: 연도
 				2. date.getMonth()		: 월 [0~11 / 보통 +1 처리 함]
