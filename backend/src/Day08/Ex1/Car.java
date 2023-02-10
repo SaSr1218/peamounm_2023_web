d732d7838b7fe9d3a76ff9fcada559f57688f0ea
Sengah Choi <cntjr3@naver.com>
2023-02-10 11:24:52 +0900

    Day28 java8일차_필드, 생성자, 메소드 예시

---
+++ backend/src/Day08/Ex1/Car.java
@@ -0,0 +1,44 @@
+// 2. 클래스 설계 목적
+
+// 클래스 선언
+	// 1. 첫글자 대문자인 영문, 띄어쓰기 X, 숫자로 시작X
+
+package Day08.Ex1;
+
+public class Car {
+	
+	// 1.필드
+	String model;
+	String color;
+	int maxSpeed;
+	
+	// 2. 생성자
+		// 1. 생성자가 1개도 선언이 없을때 기본생성자 사용가능
+		// 2. 생성자 이름은 클래스명과 동일! 다르면 메소드[함수]가 된다!
+		// 3. 기본생성자[깡통] , 풀생성자 -> 관례적으로 만들고 시작
+		// 4. 오버로딩 : 이름이 동일할 경우 시그니처(매개변수 개수 , 타입이 다를 경우)를 이용해 식별
+		// vs 오버라이딩 : 부모의 메소드를 재정의(리모델링)
+	
+
+	// 1. 반생성자 = 객체 생성시 매개변수 없다.
+	Car() { }
+	// 2. 생성자 = 2개의 매개변수를 받는다.
+	Car ( String model, String color){
+		this.model = model;		// this.내부필드명 = 매개변수
+		this.color = color;
+		
+	}
+	// 3. 생성자 = 3개의 매개변수를 받는다.
+	Car ( String model, String color, int maxSpeed ) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	// 3. 메소드
+	@Override
+	public String toString() {
+		return "Car [model=" + model + ", color=" + color + ", maxSpeed=" + maxSpeed + "]";
+	}
+	
+}
---
+++ backend/src/Day08/Ex1/실행.java
@@ -0,0 +1,29 @@
+package Day08.Ex1;
+
+public class 실행 {
+	public static void main(String[] args) {
+		// 1. Car 클래스를 이용한 객체 생성
+		// 클래스명 변수명 = new 생성자명();
+		
+		 // 2,3 번을 써야함. 1번은 다이렉트. 유효성검사가 안됨. 문제 확률 높아짐
+		 // 2,3번의 경우 유효성검사를 해주고 통과하면 this로 값을 받으면 된다.
+		
+		// 1. 기본생성자                        
+		Car car = new Car();					
+		car.model="그랜저"; car.color="노랑";
+		System.out.println( car.toString() );
+
+		// 2. 2개의 매개변수 생성자
+		Car car3 = new Car("그랜저", "파랑");
+		System.out.println( car3.toString() ); // .toString() = 객체 주소값 할당
+		
+		// 3. 3개의 매개변수 생성자
+		Car car2 = new Car("그랜저", "검정", 250);
+		System.out.println( car2.toString() );
+		
+		
+		
+		
+	}
+
+}
