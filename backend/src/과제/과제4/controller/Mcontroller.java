cc263f581504171a52bcd2a8382db85aca4ead2a
Sengah Choi <cntjr3@naver.com>
2023-02-10 16:58:14 +0900

    Day 28 java8일차_과제4 회원가입, 로그인, 아이디찾기, 비밀번호찾기 기능

---
+++ backend/src/과제/과제4/controller/Mcontroller.java
@@ -0,0 +1,72 @@
+package 과제.과제4.controller;
+
+import java.util.ArrayList;
+
+import 과제.과제4.model.Member;
+
+// * 처리/제어 담당 (함수)
+public class Mcontroller {
+	// * DB 대신할 [ 임시 데이터 저장소 만들기 ]
+	ArrayList<Member> memberDb = new ArrayList<>();
+	
+	// 1. 회원가입 로직
+	public int signup( String id , String pwd , String confirmpwd , String name , String phone ) {
+		// 1. 유효성검사
+		if( !pwd.equals(confirmpwd) ) { return 1; } // 회원가입실패 = 1번 
+		// 2. 객체 만들기
+		Member member = new Member(id, pwd, name, phone);
+		// 3. db처리 [ 리스트 ]
+		memberDb.add(member);
+		return 0; // 회원가입성공 = 0번 . 이때 사용하는 것이 return(반환)
+	}
+	// 2. 로그인 로직
+	public int login( String id , String pwd  ) {
+		
+		// * 모든 멤버들 중 동일한 아이디/비밀번호 찾기
+		for ( int i = 0 ; i<memberDb.size(); i++ ) {
+			if( memberDb.get(i).id.equals(id) ) {
+				// 비밀번호가 같으면
+				if ( memberDb.get(i).pwd.equals(pwd) ) {
+					return i; // 회원번호[인덱스]
+				}else {
+					return -1; // index를 쓰고 있어서 0이하를 써야함.
+				}
+			} // if end
+		} // for end
+		return -2; // 아이디가 없음 // for문 다 방문했지만 동일한 아이디가 없었다.
+	}
+
+	// 3. 아이디 찾기 로직
+	public String findId( String name, String phone ) {
+	
+		for ( int i = 0 ; i<memberDb.size(); i++ ) {
+			// 이름이 같으면
+			if(memberDb.get(i).name.equals(name)) {
+				// 이름 같고 번호도 같으면
+				if(memberDb.get(i).phone.equals(phone)) {
+					return "찾은 아이디 : " + memberDb.get(i).id;
+				} else {	
+					return "전화번호가 틀립니다"; // 전화번호 오류
+				}
+			}
+		}// for e		
+	return "아이디가 다릅니다."; // 아이디 다름
+	}
+	
+	// 4. 비밀번호 찾기 로직
+	public String findPw( String id, String phone ) {
+		
+		for ( int i = 0 ; i<memberDb.size(); i++ ) {
+			// id 같으면
+			if(memberDb.get(i).id.equals(id)) {
+				// id 같고 번호도 같으면
+				if(memberDb.get(i).phone.equals(phone)) {
+					return "찾은 비밀번호 : " + memberDb.get(i).pwd;
+				} else {	
+					return "전화번호가 틀립니다"; // 전화번호 오류
+				}
+			}
+		}// for e		
+	return "id가 다릅니다."; // 아이디 다름
+	}
+}
---
+++ backend/src/과제/과제4/view/Front.java
@@ -0,0 +1,83 @@
+package 과제.과제4.view;
+// * 입출력 : print , scanner (제어문, 반복문만 돌림, 실제 기능 x)
+
+import java.util.Scanner;
+
+import 과제.과제4.controller.Mcontroller;
+import 과제.과제4.model.Member;
+
+public class Front {
+	
+	Scanner scanner = new Scanner(System.in);
+	Mcontroller mc = new Mcontroller();	// 각각 선언하면 번지가 다름! 공유하려면 위에 써야함.
+
+	
+	// 1. 메인페이지
+	public void index() {
+		while(true) {
+		System.out.println("1. 회원가입 2. 로그인 3. 아이디찾기 4. 비밀번호찾기");
+		int ch = scanner.nextInt();
+		if( ch == 1 ) { signup(); }
+		else if( ch == 2 ) { login(); }
+		else if( ch == 3 ) { findId(); }
+		else if( ch == 4 ) { findPw(); }
+		else { System.err.println("잘못된 메뉴선택입니다.");}
+		}
+	}
+	
+	// 2. 회원가입 페이지
+	void signup() {
+		System.out.println("아이디 : ");		String id = scanner.next();
+		System.out.println("비밀번호 : ");		String pwd = scanner.next(); 
+		System.out.println("비밀번호 확인 : ");	String confirmpwd = scanner.next();
+		System.out.println("이름 : ");		String name = scanner.next();
+		System.out.println("전화번호 : ");		String phone = scanner.next();
+		
+		int result = 
+				mc.signup( id , pwd, confirmpwd , name , phone );
+		if ( result == 1 ) {
+			System.out.println("[회원가입 실패]");
+		} else if( result == 0 ) {
+			System.out.println("[회원가입 성공]");
+		}
+	}
+	
+	// 3. 로그인 페이지
+	void login() {
+		System.out.println("ID : "); String id = scanner.next();
+		System.out.println("PW : "); String pwd = scanner.next();
+		
+		int result = mc.login( id, pwd );
+		if ( result >= 0 ) {
+			System.out.println("로그인 성공");
+			// 게시판으로 이동
+		}else if ( result == -1) {
+			System.out.println("비밀번호가 다릅니다.");
+		} else if ( result == -2) {
+			System.out.println("없는 회원입니다.");
+		}
+	}
+	// 4. 아이디 찾기 페이지
+	void findId() {
+		System.out.println("이름 : ");	String name = scanner.next();
+		System.out.println("전화번호 : ");	String phone = scanner.next();
+		
+		String result = mc.findId(name, phone);
+		System.out.println( result );
+
+	}
+
+	// 5. 비밀번호 찾기 페이지
+	void findPw() {
+		System.out.println("아이디 : ");	String id = scanner.next();
+		System.out.println("전화번호 : ");	String phone = scanner.next();
+	
+		String result = mc.findPw( id, phone );
+		System.out.println( result );
+	}
+	
+	
+	
+	
+	
+} // class e
