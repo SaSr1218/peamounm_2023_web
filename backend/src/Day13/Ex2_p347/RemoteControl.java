2871d6877cfda1445db5c746fb8b2f41a339c346
Sengah Choi <cntjr3@naver.com>
2023-02-20 17:24:43 +0900

    Day31 인터페이스2

--- backend/src/Day13/Ex2_p347/RemoteControl.java
+++ backend/src/Day13/Ex2_p347/RemoteControl.java
@@ -1,5 +1,10 @@
 package Day13.Ex2_p347;
 
 public interface RemoteControl {
-
+	int MAX_VOLUME = 10;
+	int MIN_VOLUME = 0;
+	
+	public abstract void turnOn();
+	void turnOff();
+	void setVolume(int volume);
 }
