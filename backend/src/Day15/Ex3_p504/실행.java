e848c67137f5f8fb41889974784a2e1d38908346
Sengah Choi <cntjr3@naver.com>
2023-02-21 10:55:05 +0900

    Day 35 toString

--- backend/src/Day15/Ex3_p504/실행.java
+++ backend/src/Day15/Ex3_p504/실행.java
@@ -1,5 +1,13 @@
 package Day15.Ex3_p504;
 
 public class 실행 {
-
+	public static void main(String[] args) {
+		
+		SmartPhone myPhone = new SmartPhone("삼성전자" , "안드로이드");
+		
+		System.out.println( myPhone.toString() );
+		
+		System.out.println( myPhone ); // 객체 출력 기본값이 toSring() 포함임!
+		
+	}
 }
