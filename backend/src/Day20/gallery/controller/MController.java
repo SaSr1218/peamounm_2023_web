3def2b93b35cdb5600980f696e296cb126543ff8
Sengah Choi <cntjr3@naver.com>
2023-02-28 11:44:28 +0900

    Day40 java 20일차 아이디 중복체크, 회원가입

--- backend/src/Day20/gallery/controller/MController.java
+++ backend/src/Day20/gallery/controller/MController.java
@@ -1,5 +1,32 @@
 package Day20.gallery.controller;
 
-public class MController {
+import Day20.gallery.model.Dao.MemberDao;
+import Day20.gallery.model.Dto.MemberDto;
 
-}
+public class MController {
+	
+	private static MController mcon = new MController();
+	private MController() { }
+	public static MController getInstance() { return mcon; }
+		
+	// 1. 회원가입 처리 [ 아이디중복체크 ]
+	public int signup ( String mid , String mpw , String mname , String mphone ) {
+			
+		// 1. 유효성검사 [ 글자수 , 중복체크 등등 ]
+			// 1. 아이디 중복체크
+		if ( MemberDao.getInstance().idCheck(mid) ) {
+			return 2;	// 중복된 아이디입니다. 함수 종료
+		}
+		// 2. 객체화
+		MemberDto dto = new MemberDto(0, mid, mpw, mname, mphone);
+		
+		// 3. 회원가입 DB처리
+		return MemberDao.getInstance().signup(dto);
+		}
+		
+	
+
+	
+
+	}
+
--- backend/src/Day20/gallery/model/Dao/MemberDao.java
+++ backend/src/Day20/gallery/model/Dao/MemberDao.java
@@ -1,9 +1,47 @@
 package Day20.gallery.model.Dao;
 
+import Day20.gallery.model.Dto.MemberDto;
+
 public class MemberDao extends Dao{
 
 	// 1. 싱글톤
 	private static MemberDao dao = new MemberDao();
 	private MemberDao() { }
 	public static MemberDao getInstance() { return dao; }
+	
+	// 기능 메소드
+	
+	// 1. 아이디 중복체크
+	public boolean idCheck ( String mid ) {
+		String sql = "select * from member where mid = ?";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, sql);
+			rs = ps.executeQuery();
+			// 검색된 레코드가 있으면 [ 중복된 아이디 ]
+			if ( rs.next() ) { return true; }
+			// 없으면 [중복된 아이디가 없다 ]
+			else { return false; }
+			
+		}catch (Exception e) { System.out.println(e);}
+		return true; // 그외
+	}
+	
+	// 2. 회원가입
+	public int signup( MemberDto dto ) {
+		String sql = "insert into member ( mid , mpw , mname , mphone ) values ( ? , ? , ? , ? )";
+		try {
+			ps = con.prepareStatement(sql);
+			ps.setString(1, dto.getMid() );
+			ps.setString(2, dto.getMpw() );
+			ps.setString(3, dto.getMname() );
+			ps.setString(4, dto.getMphone() );
+			ps.executeUpdate();
+			return 1; // 성공
+		}catch (Exception e) { System.out.println(e); }
+		return 3; // db오류
+	} 
+	
+	
+	
 }
