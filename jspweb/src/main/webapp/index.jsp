1aececd14938ce52d405997e52a7179a10a30d91
Sengah Choi <cntjr3@naver.com>
2023-03-02 12:22:36 +0900

    Day41 ajax post, get 방식

--- jspweb/src/main/webapp/index.jsp
+++ jspweb/src/main/webapp/index.jsp
@@ -20,7 +20,8 @@
 	
 	
 	
-	
+	<!-- 최신 jquery [ js 라이브러리 ] 가져오기 -->
+	<script  src="http://code.jquery.com/jquery-latest.min.js"></script>
 	<!-- 사용자정의 JS 가져오기 [ 경로 : /프로젝트명/[wepapp]폴더생략/폴더/파일명 -->
 	<!--  <script src="jspwep/js/index.js" type="text/javascript"></script> -->
 	 <script src="js/index.js" type="text/javascript"></script> 
--- jspweb/src/main/webapp/js/index.js
+++ jspweb/src/main/webapp/js/index.js
@@ -1,3 +1,19 @@
+
+function 예제1(){
+	let data = document.querySelector('.inputdata').value;
+	console.log( data );
+
+	$.ajax({
+		url : "http://localhost:8080/jspweb/indextest" ,		// 통신할 서블릿 주소
+		method : "post" ,	// HTTP 메소드 
+		data : { "data" : data } ,	// 데이터 보내기
+		success : function( result ) { 
+			console.log( result ) 
+			}	// 데이터 받기
+	});
+	
+}
+
 // document
 	// querySelector( 식별자 )
 		// 1. document.querySelector('.classname')
@@ -12,9 +28,42 @@
 // JS 함수
 // function 함수명 ( 매개변수 ) { 실행코드 }
 
+	// js --> 서블릿 이동
+	// 0. $ : jquery 표현식
+	// 1. ajax 메소드 사용 : $.ajax( );
+	// 2. ajax 매개변수[속성:객체형태] : { } --> $.ajox( { } )
+	// 3. 속성
+		// 1. url : 통신할 경로[ 서블릿(클래스) 주소 : ip/프로젝트명/클래스명 ]
+			/* 
+				url : "http://localhost:8080/jspweb/indextest"
+				url : "http://192.168.56.1:8080/jspweb/indextest"
+				url : "/jspwep/Indextest"
+			*/
+			
+		// 2. method : http 메소드방식
+			/* 
+				get
+				post 
+			*/
+			
+		// 3. data : 통신할때 보낼 데이터
+			/* 
+				js객체 형태 : { 매개변수명1 : 데이터 , 매개변수명2 : 데이터 ~~ }
+			*/
+			
+		// 4. succcess : 통신후 응답 데이터 받기
+			/*
+				1. success : 함수명 ( 매개변수 ){ }
+				2. success : ( ) => { }
+			 */
+		
+	/* $.ajax({ 
+			url : "jspweb/indextest" , 
+			method : "" , 
+			data : { 매개변수명 : 데이터 } , 
+			success : function( result ) {
+				
+			} 
+		});
 
-function 예제1(){
-	let data = document.querySelector('.inputdata').value;
-	console.log( data );
-	
-}
+	*/
