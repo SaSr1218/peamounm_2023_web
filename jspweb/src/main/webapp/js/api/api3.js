d18e6b8dc39b48a18934fd35f7cc32681e1af765
Sengah Choi <cntjr3@naver.com>
2023-03-21 22:09:35 +0900

    Day54 jsp 14일차 [ 클러스터 ajax 해결, get 방식 오류 ]

--- jspweb/src/main/webapp/js/api/api3.js
+++ jspweb/src/main/webapp/js/api/api3.js
@@ -18,9 +18,6 @@ kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
     // 클릭한 위도, 경도 정보를 가져옵니다 
     var latlng = mouseEvent.latLng; 
     
-    // 마커 위치를 클릭한 위치로 옮깁니다
-    marker.setPosition(latlng);
-    
     var message = '클릭한 위치의 위도는 ' + latlng.getLat() + ' 이고, ';
     message += '경도는 ' + latlng.getLng() + ' 입니다';
     
@@ -39,6 +36,9 @@ var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
     markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
 
 
+// 주소-좌표 변환 객체를 생성합니다
+var geocoder = new kakao.maps.services.Geocoder();
+
 // ------------- 마커 클러스터 생성 ------------------ //
  
    // 마커 클러스터러를 생성합니다 
@@ -50,14 +50,11 @@ var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
 
 $.get(
 	"https://api.odcloud.kr/api/3035882/v1/uddi:5fae3cf5-bc15-4eba-87d8-8289b74e659b_201912202015?page=1&perPage=292&serviceKey=plPlvDiu46sxWDa0HDwHx%2B3YNZWUvt1S8CfVeP8A9Mo1n7ZGY0D8rZd8YaU5acCrIRuu1xmOiqEq7p2EeDxRYw%3D%3D" ,
-	
-	function(r){
-		console.log(r.data);
 
-			let markers = $(r.data).map(function ( i , o ) {
+		(r) =>{
+			$(r.data).map(function ( i , o ) {
 				console.log( o.주소 )
-							// 주소-좌표 변환 객체를 생성합니다
-				var geocoder = new kakao.maps.services.Geocoder();
+
 				
 				// 주소로 좌표를 검색합니다
 				geocoder.addressSearch(o.주소, function(result, status) {
@@ -74,31 +71,73 @@ $.get(
 				            image : markerImage
 				        });
 				        
-				        console.log(marker)
 // ----------------------- 마커 클릭 이벤트  ------------------------------------ //  
 						// 위에서 생성된 마커객체의 클릭 이벤트 추가하기
 						kakao.maps.event.addListener(marker, 'click', function() {
 						      
-						      // 모달 정보 담기
-						      document.querySelector('.modal_title').innerHTML = o.주소;
-						      document.querySelector('.modal_title').style.fontSize = '15px';
-						      document.querySelector('.modal_content').innerHTML = o.약국명;
+						// 모달 정보 담기
+						document.querySelector('.modal_title').innerHTML = o.주소;
+						document.querySelector('.modal_title').style.fontSize = '15px';
+						document.querySelector('.modal_content').innerHTML = o.약국명;
 						      
-    						      
-						      // 모달 띄우기
-						      openModal(); 
+						// 모달 띄우기
+						openModal(); 
 						}); 				        
-				        return marker;
-				    } 
+			 			clusterer.addMarkers(marker);	
+
+				    } // if end 
+				    
 				});
-				
+
 			}); // map end
 	        // 클러스터러에 마커들을 추가합니다
-	        clusterer.addMarkers(markers);	
-	});
+		 // success end
+}); // ajax end
 
 
-
+/* ajax 구동방법
+$.ajax({
+	url : "https://api.odcloud.kr/api/3035882/v1/uddi:5fae3cf5-bc15-4eba-87d8-8289b74e659b_201912202015?page=1&perPage=292&serviceKey=plPlvDiu46sxWDa0HDwHx%2B3YNZWUvt1S8CfVeP8A9Mo1n7ZGY0D8rZd8YaU5acCrIRuu1xmOiqEq7p2EeDxRYw%3D%3D",
+	method : "get",
+	async : false,
+	success : (r) => {
+		
+		// 주소-좌표 변환 객체를 생성합니다
+		var geocoder = new kakao.maps.services.Geocoder();
+		r.data.forEach( (o) =>{
+		
+				// 주소로 좌표를 검색합니다
+				geocoder.addressSearch( o.주소, function(result, status) {
+					
+				    // 정상적으로 검색이 완료됐으면 
+				     if (status === kakao.maps.services.Status.OK) {
+				    
+				    	// 결과값으로 받은 위치를 마커로 표시합니다
+				        let marker =  new kakao.maps.Marker({
+				            position : new kakao.maps.LatLng(result[0].y, result[0].x),
+				            image : markerImage
+				        });
+				        
+// ----------------------- 마커 클릭 이벤트  ------------------------------------ //  
+						// 위에서 생성된 마커객체의 클릭 이벤트 추가하기
+						kakao.maps.event.addListener(marker, 'click', function() {
+						      
+						// 모달 정보 담기
+						document.querySelector('.modal_title').innerHTML = o.주소;
+						document.querySelector('.modal_title').style.fontSize = '15px';
+						document.querySelector('.modal_content').innerHTML = o.약국명;
+						      
+						// 모달 띄우기
+						openModal(); 
+						});
+				        
+				        clusterer.addMarker(marker);
+				    } // if end 
+				}); // geo end
+		}) // forEach end
+	} // success end 
+}) // ajax end
+ */
 
 
 
