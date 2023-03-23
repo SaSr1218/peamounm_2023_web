764b798a6a30eb28bb62fc277066ebf23b26cfc0
Sengah Choi <cntjr3@naver.com>
2023-03-23 12:34:26 +0900

    Day56 jsp 16일차 [ 채팅 입장, 나가기, 이모티콘 구현 완료 ]

--- jspweb/src/main/webapp/board/chatting.jsp
+++ jspweb/src/main/webapp/board/chatting.jsp
@@ -12,29 +12,49 @@
 <body>
 
 	<%@include file = "/header.jsp" %>
-	<div class="container">
-	
-		<div class="chattingbox"> <!-- 채팅 구역 -->
+	<div class="container chattingwrap">
+		
+		<div class="conlistbox"> <!-- 접속 명단 표시 구역 -->
+
+		</div>
+		
+		<div class="chattingbox">	<!--  채팅 구역 -->
+		
+			<div class="contentbox"> <!-- 채팅창  -->
+				
+				
+			</div>
 			
-				<div class="contentbox"> <!-- 채팅창 -->
+			<!--  form-control : bs -->
+			<textarea onkeyup="enterkey()" class="msgbox" rows="" cols=""></textarea>
+			
+			<div class="chattingbtnbox">
+			
+				<!-- 
+					bs : 드롭다운
+						
+				 -->
+			
+				<!-- 드롭다운 버튼 -->
+				<button class="emobtn" type="button" data-bs-toggle="dropdown">
+					<i class="far fa-smile"></i>
+				</button>
+				
+				<!-- 드롭다운 버튼 클릭시 보이는 구역 -->
+				<div class="dropdown-menu enolist">
 					
-					<!-- 보낼 때 -->
-
-					<!--  알람 -->
-
-					<!--  받을때 [ 프로필 , 시간 , 내용 -->
-
+					
 				</div>
 				
-				<!-- form-control msgbox : bs -->
-				<textarea onkeyup="enterkey()" class="msgbox" rows="" cols=""></textarea>
-				
-				<button onclick="보내기()" class="sendbtn" type="button"> 보내기 </button>
-					
-		</div>
+				<button onclick="보내기()" class="sendbtn" type="button">보내기</button>
+			</div>
 			
-							
+			
+			
 		</div>
+	
+		
+	</div>
 	
 	
 	<script src="/jspweb/js/board/chatting.js" type="text/javascript"></script>
--- jspweb/src/main/webapp/js/board/chatting.js
+++ jspweb/src/main/webapp/js/board/chatting.js
@@ -43,18 +43,39 @@ if(memberInfo.mid == null){
 }
 
 // 2. 클라이언트소켓이 접속했을때 하고 싶은 이벤트/함수 정의 [ 따로 작성할 필요가 없으면 하지 않아도 됨. ] 
-function 서버소켓연결(){ contentbox.innerHTML += `
-					<div class="alarm">
-						<span> 채팅방 입장 하셨습니다. </span>
-					</div>` } 
+function 서버소켓연결(){ 
+	
+	자료보내기( memberInfo.mid+"님 이 채팅방에 접속하셨습니다." , "alarm" )
+	
+} 
 
 // 3. 클라이언트소켓이 서버에게 메시지를 보내기 [ @OnMessage ]
 function 보내기(){
 	let msgbox = document.querySelector('.msgbox').value
-	// * 메시지 전송하기 *
-	클라이언트소켓.send( msgbox );	// ---> @OnMessage
+	// *  서버소켓에게 메시지 전송하기 *
+		// JSON형식의 문자열 타입 만들어서 문자열로 타입으로 전송
+		// JSON.parse ( JSON형식의 문자열타입 )  : 문자열타입 --> JSON 타입으로 변환
+		// JSON.stringify ( JSON 객체 )		: JSON타입 --> JSON 형식[모양]의 String 타입으로 변환
+		let msg = {
+			type : 'msg' ,
+			msgbox : msgbox
+		}
+	클라이언트소켓.send( JSON.stringify( msg ) );	// ---> @OnMessage
 	
 	document.querySelector('.msgbox').value = '';
+}
+
+// 4-2 type 에 따른 출력 html 구별
+function 메시지타입구분 ( msg ) {
+	let json = JSON.parse( msg );
+	
+	let html = '';
+	if (json.type == 'msg'){
+		html += `<div class="content"> ${json.msgbox} </div>`
+	}else if ( json.type == 'emo') {
+		html += `<div class="content emocontent"> <img src="/jspweb/img/imoji/emo${json.msgbox}.gif"> </div>`
+	}
+	return html;
 }
 
 // 4. 서버로부터 메시지가 왔을때 메시지 받기 -> 매개변수 필수! 여기에선 e 를 말함
@@ -63,23 +84,48 @@ function 메시지받기(e) { // <---- getBasicRemote().sendText(msg)
 	console.log(e.data) // 문자열 json -> 객체 json 형변환 시켜야함!!
 	console.log( JSON.parse( e.data ) ); // JSON.parse ( json으로 형변환 시킬 데이터 );
 	
-	let data = JSON.parse(e.data);	// 전달받은 메시지 dto
+	let data = JSON.parse( e.data );	// 전달받은 메시지 dto
 	
-	// 보낸사람과 현재 유저와 일치하면 [ 내가 보낸 메시지 ]
-	if ( data.frommid == memberInfo.mid ){
+	// let msg = JSON.parse ( data.msg ) ; // 명단에는 data.msg가 존재 X 그래서 주석 처리 후 96, 100번째 줄을 타입변환시킴 원래는 msg.msgbox임
+	
+	// 명단[여러개=list/Array] vs 메시지정보[1개=dto/object]
+		// Array 타입 확인 : Array.isArray( 객체 ) : 해당 객체가 배열/리스트이면 true
+	if( Array.isArray( data ) ){
+		let html = '';
+		data.forEach( (o) => {
+			html += `
+					<div class="conbox"> <!-- 접속 명단 1명 기준 -->
+						<div> <img alt="" src="/jspweb/member/mimg/${ o.fromimg == null ? 'default.webp' : o.fromimg}" class="hpimg"> </div>
+						<div class="name"> ${o.frommid} </div>				
+					</div>
+					`
+		})
+		document.querySelector('.conlistbox').innerHTML = html;
+	}
+	else if ( JSON.parse( data.msg ).type == 'alarm'){
+			
+		contentbox.innerHTML += `
+						<div class="alarm">
+							<span> ${ JSON.parse( data.msg ).msgbox } </span>
+						</div>` 
+	} 
+	// 보낸 사람과 현재 유저와 일치하면 [ 내가 보낸 메시지 ]
+	else if ( data.frommid == memberInfo.mid ) {
 		contentbox.innerHTML += `
 							<div class="secontent">
 								<div class="date"> ${data.time} </div>
-								<div class="content"> ${data.msg} </div>
-							</div>`
-	}else{ // [ 내가 받은 메시지 ]
+								${메시지타입구분( data.msg ) }
+							</div>`		
+	}
+
+	else{ // [ 내가 받은 메시지 ]
 		contentbox.innerHTML += `
 							<div class="tocontent">
 								<div><img src="/jspweb/member/mimg/${ data.fromimg == null ? 'default.webp' : data.fromimg }" class="hpimg"></div>
 								<div class="rcontent">
 									<div class="name"> ${data.frommid} </div>
 									<div class="contentdate">
-										<div class="content"> ${data.msg} </div>
+										${메시지타입구분( data.msg ) }
 										<div class="date"> ${data.time} </div>
 									</div>
 								</div>
@@ -98,6 +144,10 @@ function 메시지받기(e) { // <---- getBasicRemote().sendText(msg)
 // 5. 서버와 연결이 끊겼을 때 [ 따로 작성할 필요가 없으면 하지 않아도 됨. ]
 function 연결해제(e){
 	console.log('연결해제')
+	// 이미 세션이 종류 후에 발생하는 함수이므로 아래 코드는 다른 세션에게 전달 불가능
+	// -> 그래서 서버가 종료 코드를 보내줘야함 Chatting 60 번째 line
+	// 자료보내기( memberInfo.mid+"님 이 채팅방에 나갔습니다." , "alarm" )
+	
 }
 
 // 6. 엔터키를 눌렀을때
@@ -107,6 +157,24 @@ function enterkey() {
 	}
 }
 
+// 7. 이모티콘 출력
+getemo();
+function getemo(){
+	let html = '';
+	for ( let i = 0 ; i<=43; i++ ){
+		html += `<img onclick="자료보내기(${i} , 'emo')" alt="" src="/jspweb/img/imoji/emo${i}.gif" width="70px">`
+	}
+	document.querySelector('.enolist').innerHTML = html;
+}
+
+// 8. 타입형 메시지 전송하기
+function 자료보내기( msgbox , type ){
+		let msg = {
+			type : type ,
+			msgbox : msgbox
+		}	
+	클라이언트소켓.send( JSON.stringify( msg ) ); // ---> @OnMessage
+}
 
 /* 
 	클라이언트 소켓 통신 결과
